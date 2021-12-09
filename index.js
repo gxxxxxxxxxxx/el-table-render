2f950ac31675ffdc3433c108f3986e1d588fd199
ganxiaoxing <ganxiaoxing@junrunrenli.com>
2021-12-09 11:52:27 +0800

    feat: support column hidden

--- index.js
+++ index.js
@@ -3,37 +3,43 @@ export default {
   props: {
     columns: {
       type: Array,
-      default: () => []
-    }
+      default: () => [],
+    },
   },
   render(h, ctx) {
     return h(
-      'el-table',
+      "el-table",
       {
         props: {
-          ...ctx.data.attrs
+          ...ctx.data.attrs,
         },
         on: ctx.data.on,
         ref: ctx.data.ref,
-        directives: ctx.data.directives || []
+        directives: ctx.data.directives || [],
       },
-      ctx.props.columns.map(column =>
-        h('el-table-column', {
-          props: {
-            ...column
-          },
-          scopedSlots: column.type
-            ? {}
-            : {
-                default: props => {
-                  return column.render ? column.render(h, props) : props.row[column.prop]
+      ctx.props.columns
+        .filter((item) => !item.hide)
+        .map((column) =>
+          h("el-table-column", {
+            props: {
+              ...column,
+            },
+            scopedSlots: column.type
+              ? {}
+              : {
+                  default: (props) => {
+                    return column.render
+                      ? column.render(h, props)
+                      : props.row[column.prop];
+                  },
+                  header: (props) => {
+                    return column.headerRender
+                      ? column.headerRender(h, props)
+                      : props.column.label;
+                  },
                 },
-                header: props => {
-                  return column.headerRender ? column.headerRender(h, props) : props.column.label
-                }
-              }
-        })
-      )
-    )
-  }
-}
+          })
+        )
+    );
+  },
+};
