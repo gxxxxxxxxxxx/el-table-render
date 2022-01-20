aa8c2cb62088a45bbcbd8cb0c80ce3daba640d99
ganxiaoxing <ganxiaoxing@junrunrenli.com>
2022-01-20 18:05:02 +0800

    feat: 增加多表头支持

--- index.js
+++ index.js
@@ -3,43 +3,47 @@ export default {
   props: {
     columns: {
       type: Array,
-      default: () => [],
-    },
+      default: () => []
+    }
   },
   render(h, ctx) {
+    const listRender = arr => {
+      return arr
+        .filter(item => !item.hide)
+        .map(column =>
+          h(
+            'el-table-column',
+            {
+              props: {
+                ...column
+              },
+              scopedSlots: column.type
+                ? {}
+                : {
+                  default: props => {
+                    return column.render ? column.render(h, props) : props.row[column.prop]
+                  },
+                  header: props => {
+                    return column.headerRender ? column.headerRender(h, props) : props.column.label
+                  }
+                }
+            },
+            Array.isArray(column.children) && listRender(column.children)
+          )
+        )
+    }
+
     return h(
-      "el-table",
+      'el-table',
       {
         props: {
-          ...ctx.data.attrs,
+          ...ctx.data.attrs
         },
         on: ctx.data.on,
         ref: ctx.data.ref,
-        directives: ctx.data.directives || [],
+        directives: ctx.data.directives || []
       },
-      ctx.props.columns
-        .filter((item) => !item.hide)
-        .map((column) =>
-          h("el-table-column", {
-            props: {
-              ...column,
-            },
-            scopedSlots: column.type
-              ? {}
-              : {
-                  default: (props) => {
-                    return column.render
-                      ? column.render(h, props)
-                      : props.row[column.prop];
-                  },
-                  header: (props) => {
-                    return column.headerRender
-                      ? column.headerRender(h, props)
-                      : props.column.label;
-                  },
-                },
-          })
-        )
-    );
-  },
-};
+      listRender(ctx.props.columns)
+    )
+  }
+}
--- types/index.d.ts
+++ types/index.d.ts
@@ -3,6 +3,7 @@ import { VNode } from "vue";
 export interface column extends TableColumn {
   render: () => VNode;
   headerRender: () => VNode;
+  children: column;
 }
 
 export default class ElTableRender extends Table {
